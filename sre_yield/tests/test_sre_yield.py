547a213a151f274c98a453a4821a1e725b5594ab
John Vandenberg <jayvdb@gmail.com>
2020-09-24 20:55:17 +0700

    Add tests for unsupported sequence operations

--- sre_yield/tests/test_sre_yield.py
+++ sre_yield/tests/test_sre_yield.py
@@ -192,6 +192,17 @@ class YieldTest(unittest.TestCase):
             sre_yield.AllStrings("x|y").get_item(-3)
         self.assertEqual(str(cm.exception), "Index -3 out of bounds")
 
+    def testUnsupportedErrors(self):
+        parsed = sre_yield.AllStrings("x")
+        self.assertSequenceEqual(parsed, ["x"])
+        with self.assertRaises(TypeError) as cm:
+            parsed[:] = ["a"]
+        self.assertEqual(str(cm.exception), "'RegexMembershipSequence' object does not support item assignment")
+
+        with self.assertRaises(TypeError) as cm:
+            del parsed[0]
+        self.assertEqual(str(cm.exception), "'RegexMembershipSequence' object doesn't support item deletion")
+
     def testContains(self):
         parsed = sre_yield.AllStrings("[01]+")
         self.assertTrue("0101" in parsed)
